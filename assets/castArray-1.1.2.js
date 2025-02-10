d9790b98aa4c252396128f54705391756e6a8b37
wangliyuan <zpzxgcr@163.com>
2025-02-10 23:44:07 +0800

    update json 1.1.2

---
+++ assets/castArray-1.1.2.js
@@ -0,0 +1,1 @@
+import{bP as a}from"./entry-1.1.2.js";function e(){if(!arguments.length)return[];var r=arguments[0];return a(r)?r:[r]}export{e as c};
--- assets/pen-paper-1.1.1.js
+++
@@ -1,1 +0,0 @@
-import"./entry-1.1.1.js";const r=""+new URL("pen-paper-1.1.1.png",import.meta.url).href;export{r as _};
